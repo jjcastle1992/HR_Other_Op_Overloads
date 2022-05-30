97ca22eecc489cabb9b92deb662b0731913e63ed
James <jjcastle@utexas.edu>
2022-05-30 16:25:45 -0500

    Added first stab at the + operator overload for class Complex to add complex (real and imaginary) numbers together.

--- main.cpp
+++ main.cpp
@@ -35,6 +35,27 @@ public:
 //Overload operators + and << for the class complex
 //+ should add two complex numbers as (a+ib) + (c+id) = (a+c) + i(b+d)
 //<< should print a complex number in the format "a+ib"
+Complex operator + (Complex x, Complex y) {
+    string concatNum = "";
+    int num1 = 0;
+    int num2 = 0;
+    int newNum = 0;
+    int imaginary1 = 0;
+    int imaginary2 = 0;
+    int newImaginary = 0;
+    Complex newComplex;
+
+    num1 = x.a;
+    num2 = y.a;
+    newNum = num1 + num2;
+    imaginary1 = x.b;
+    imaginary2 = y.b;
+    newImaginary = imaginary1 + imaginary2;
+    newComplex.a = newNum;
+    newComplex.b = newImaginary;
+
+    return newComplex;
+}
 
 int main()
 {
@@ -45,5 +66,5 @@ int main()
     x.input(s1);
     y.input(s2);
     Complex z=x+y;
-    cout<<z<<endl;
+//    cout<<z<<endl;
 }
